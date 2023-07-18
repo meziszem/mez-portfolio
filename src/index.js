9dd5773bc6cea132ffb74415942e3d47b6b84c2c
meziszem <meseretfh@gmail.com>
2023-07-18 13:00:57 -0400

    created new branch for splash page feature

--- src/index.js
+++ src/index.js
@@ -5,3 +5,10 @@ import { targetElements, defaultProps } from "./data/scrollRevealConfig";
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
 
+var splashScreen = document.querySelector('.splash-container');
+splashScreen.addEventListener('click',()=>{
+  splashScreen.style.opacity = 0;
+  setTimeout(()=>{
+    splashScreen.classList.add('hidden')
+  },610)
+})
