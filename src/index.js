51a85d69d043452dcfef2ccfc40c944ca62a70a2
meziszem <meseretfh@gmail.com>
2023-07-28 15:25:17 -0400

    more splash screen tweaks

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import { targetElements, defaultProps } from "./data/scrollRevealConfig";
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
 
-var splashScreen = document.querySelector('.splash-container');
+const splashScreen = document.querySelector('.splash-container');
+
 splashScreen.addEventListener('click',()=>{
-  splashScreen.style.opacity = 0;
   setTimeout(()=>{
-    splashScreen.classList.add('hidden')
-  },610)
+    splashScreen.classList.add('hidden');
+  },20)
 })
